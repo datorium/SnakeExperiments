ee9d657c9fa2f0e49a5d897d09e9aa2fc6866c40
Datorium <hello@datorium.lv>
2020-06-05 17:23:42 +0300

    Add project files.

---
+++ Game.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SnakeExperiments
+{
+    public partial class Game : Form
+    {
+        Snake snake = null;
+        
+        public Game()
+        {
+            InitializeComponent();
+            InitializeGame();
+        }
+
+        private void InitializeGame()
+        {
+            snake = new Snake(this);
+            snake.Render();
+        }
+    }
+}
---
+++ Snake.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SnakeExperiments
+{
+
+    class Snake
+    {
+        private List<PictureBox> snakePixels = new List<PictureBox>();
+        private Form gameForm;
+
+        public Snake(Form form)
+        {
+            InitializeSnake();
+            gameForm = form;
+        }
+
+        private void InitializeSnake()
+        {
+            AddPixel(200, 200);
+            AddPixel(220, 200);
+            AddPixel(240, 200);
+            AddPixel(240, 220);
+            AddPixel(240, 240);
+            AddPixel(240, 260);
+        }
+
+        public void AddPixel(int left, int top)
+        {
+            PictureBox sp = new PictureBox()
+            {
+                BackColor = Color.Blue,
+                Height = 20,
+                Width = 20,
+                Left = left,
+                Top = top
+            };
+            snakePixels.Add(sp);
+        }
+
+        public void Render()
+        {
+            foreach(var sp in snakePixels)
+            {
+                gameForm.Controls.Add(sp);
+            }
+        }
+    }
+}
